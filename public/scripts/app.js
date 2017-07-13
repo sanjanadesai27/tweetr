9b966080ef9dc177437e2262d2ddbfadbb76f449
Sanjana Desai <sanjana_desai@live.ca>
2017-07-13 17:31:53 +0000

    updated app.js script to load new tweets without refresh

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
-var data = [
+  loadTweets();
+
+  var data = [
   {
     "user": {
       "name": "Newton",
@@ -25,51 +27,103 @@ var data = [
         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
       },
       "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      "content": {
+        "text": "Je pense , donc je suis"
       },
-      "handle": "@johann49"
+      "created_at": 1461113959088
     },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+    ];
 
-function renderTweets(data) {
-  data.forEach(function(tweet,i){
-    var $tweet = createTweetElement(tweet,i);
-    $('#tweetList').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  });
-}
+    function createTweetElement(tweet) {
 
-function createTweetElement(tweet,i) {
-var html = `
-  <article class="tweet">
-    <header>
-      <img src="${data[i].user.avatars.small}"/>
-      <h3> ${data[i].user.name} </h3>
-      <span> ${data[i].user.handle} </span>
-    </header>
-    <div class="content"> ${data[i].content.text} </div>
-    <footer>
-      <span>${data[i].created_at}</span>
-    </footer>
- </article> `
-return html;
-}
+      var html = `
+      <article class="tweet">
+      <header>
+      <img src="${tweet.user.avatars.small}"/>
+      <h3> ${tweet.user.name} </h3>
+      <span> ${tweet.user.handle} </span>
+      </header>
+      <div class="content"> ${tweet.content.text} </div>
+      <footer>
+      <span>${tweet.created_at}</span>
+      </footer>
+      </article> `
+      return html;
+    }
 
-renderTweets(data);
+    function renderTweets(data) {
+      let allTweets = "";
+      data.forEach(function(tweet){
+        console.log(tweet);
+        var $tweet = createTweetElement(tweet);
+        console.log($tweet);
+        allTweets = $tweet + allTweets;
+      }
+      );
+        $('#tweetList').empty().prepend(allTweets); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
-});
+      }
+
+      $('#composedTweet').submit(function(event){
+        event.preventDefault();
+        var formInput = $(this).serialize();
+        console.log(formInput);
+        var flag = false;
+
+        if(!formInput){
+          alert("hey! you need to enter something");
+          flag = true;
+        } else if (formInput.length > 140) {
+          alert("Your tweet is too long!");
+          flag = true
+        }
+        else{
+          alert('Great!');
+        }
+
+        if(!flag){
+          $.ajax({
+            method: "POST",
+            url: "/tweets",
+            data: formInput
+        }).done(() => {
+          console.log(data);
+          loadTweets();
+        });
+      }
+
+    });
+
+
+      function loadTweets() {
+       $.ajax({
+        method: "GET",
+        url: "/tweets",
+        dataType: "json",
+        success: function(data){
+          console.log(data);
+          renderTweets(data);
+        },
+        error: function(response){
+        }
+    });//ajax call
+     }
+
+
+
+   });
